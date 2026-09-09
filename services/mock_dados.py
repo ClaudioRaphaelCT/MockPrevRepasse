cb2e87fda29e2b1a741ce530c2a293aaf952d215
crteixeira <claudioraphaelct@gmail.com>
2026-09-09 09:55:22 -0300

    Ajuste dos novos campos

--- services/mock_dados.py
+++ services/mock_dados.py
@@ -13,7 +13,10 @@ def mapear_pagamento_csv(item):
         "dataHoraRepasseIF": str(item.get("dataHoraRepasseIF", "")).strip(),
         "numeroGuia": to_int(item.get("numeroGuia")),
         "competencia": to_int(item.get("competencia")),
-        "nsu": to_int(item.get("nsu"))
+        "nsu": to_int(item.get("nsu")),
+        "cpf": to_int(item.get("cpf")),
+        "matricula": str(item.get("matricula", "")).strip(),
+        "codigoTRF": to_int(item.get("codigoTRF"))
     }
 
 def gerar_mock_pagamento():
